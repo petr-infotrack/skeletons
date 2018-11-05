21d5277837c7b15f2378cb5da6d6e21041a8120d
Petr <petr.spelina@infotrack.com.au>
2018-11-05 16:40:35 +1100

    Added Tests - stubbed logging instead of Mocked (extension method issue)

---
+++ api-core-skeleton/api-net-core/api-net-core-tests/Unit/Helpers/LoggerStub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ApiNetCore.Services;
+using Microsoft.Extensions.Logging;
+
+namespace ApiNetCoreTests.Unit.Helpers
+{
+    public class LoggerStub: ILogger<ExampleService>
+    {
+        public List<Tuple<string, string>> LoggedMessages;
+
+        public LoggerStub()
+        {
+            this.LoggedMessages = new List<Tuple<string, string>>();
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            this.LoggedMessages.Add(new Tuple<string, string>(Enum.GetName(typeof(LogLevel), logLevel), state?.ToString()));
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
---
+++ api-core-skeleton/api-net-core/api-net-core-tests/Unit/Services/ExampleServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Text;
+using ApiNetCore.Services;
+using ApiNetCoreTests.Unit.Helpers;
+using Microsoft.Extensions.Logging.Internal;
+using Moq;
+using Xunit;
+
+namespace ApiNetCoreTests.Unit.Services
+{
+    public class ExampleServiceTests
+    {
+        private IExampleService service;
+
+        private LoggerStub loggerStub;
+
+        public ExampleServiceTests()
+        {
+            // logger stub is used as ILogger extension methods are difficult to Mock reliably
+
+            this.loggerStub = new LoggerStub();
+
+            this.service = new ExampleService(this.loggerStub);
+        }
+
+        [Fact]
+        public void ShouldReturnValidResult()
+        {
+            var task = this.service.CalculateTotal(1, 3);
+
+
+            Assert.Equal(4, task.Result);
+
+
+            Assert.True(this.loggerStub.LoggedMessages.Count > 0);
+
+            Assert.Contains("TRACE", this.loggerStub.LoggedMessages[0].Item1);
+            Assert.Contains("CalculateTotal methods was invoked", this.loggerStub.LoggedMessages[0].Item2);
+
+        }
+    }
+}
